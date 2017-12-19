465f8ba604c485b143a792c12f744718c8d50664
Logan Stellway <admin@thoughtfully.com>
2017-12-18 17:15:39 -0800

    Adds sequence to load module after FishPig_WordPress

--- Helper/Theme.php
+++ Helper/Theme.php
@@ -8,12 +8,12 @@ namespace LoganStellway\FishPigBedrock\Helper;
 class Theme extends \FishPig\WordPress\Helper\Theme
 {
     /**
-     * @
-    **/
+     * Get target directory
+     */
     public function getTargetDir()
     {
-        if ($this->_config->getStoreConfigValue('wordpress/setup/bedrock_enabled')) {
-            return $this->_config->getStoreConfigValue('wordpress/setup/bedrock_path') . self::DS . 'web' . self::DS . 'app' . self::DS . 'themes' . self::DS . self::THEME_NAME;
+        if ($this->config->getStoreConfigValue('wordpress/setup/bedrock_enabled')) {
+            return $this->config->getStoreConfigValue('wordpress/setup/bedrock_path') . self::DS . 'web' . self::DS . 'app' . self::DS . 'themes' . self::DS . self::THEME_NAME;
         }
         return parent::getTargetDir();
     }
